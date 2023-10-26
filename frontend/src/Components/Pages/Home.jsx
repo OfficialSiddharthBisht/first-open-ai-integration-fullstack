68c22d26f5a716a8cfdd5c8f699f91911d277edc
Siddharth <officialsiddharthbisht@gmail.com>
2023-10-26 17:40:29 +0530

    feat: final commit

--- frontend/src/Components/Pages/Home.jsx
+++ frontend/src/Components/Pages/Home.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const pixabayApi = "https://pixabay.com/api/?key=40280487-9b0305d878e51fd281dc64120&q=motivation&image_type=photo&pretty=true";
 
 const Home = () => {
-  const [content,setContent] = useState('Hello this is the content');
+  const [content,setContent] = useState('Motivational Quote');
   const [backgroudImage, setBackgroundImage] = useState("https://pixabay.com/get/gae3271e4995169a8ffbfcf0128b8b31ebfb8c2d148437f80f6cc1a796f3346b97dc06e86f62790b7c8843f824d5c08030c4a63621b8f9177f56ac3a1bc1c2565_1280.jpg");
   const randomNumber = Math.floor(Math.random() * 20);
   // large image url
@@ -38,7 +38,7 @@ const Home = () => {
     {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
     <Button colorScheme='messenger'
         onClick={() => { fetchContent(); fetchBackgroundImage(); }}
-        style={{width:"80%",margin:"auto"}}
+        style={{width:"50%",margin:"auto",fontSize:"large"}}
       >
         Give Me A Motivational Quote
       </Button>
